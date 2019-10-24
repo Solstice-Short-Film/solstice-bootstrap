078ec4d73f20353d8dff41167184d6a7c339fd3b
Tomi Poveda <tpovedatd@gmail.com>
2019-10-25 00:49:26 +0200

    Updated code to fit PEP8 standards

--- solstice/bootstrap/maya/userSetup.py
+++ solstice/bootstrap/maya/userSetup.py
@@ -12,7 +12,7 @@ __license__ = "MIT"
 __maintainer__ = "Tomas Poveda"
 __email__ = "tpovedatd@gmail.com"
 
-print('='*100)
+print('=' * 100)
 print('| Solstice Pipeline | > Loading Solstice Tools')
 
 try:
@@ -20,13 +20,13 @@ try:
     from maya import cmds
     cmds.evalDeferred('solstice_core.init()')
     print('| Solstice Pipeline | Solstice Tools loaded successfully!')
-    print('='*100)
+    print('=' * 100)
 except Exception as e:
     try:
         from solstice import core as solstice_core
         solstice_core.init()
         print('| Solstice Pipeline | Solstice Tools loaded successfully!')
-        print('='*100)
+        print('=' * 100)
     except Exception as e:
         print('ERROR: Impossible to load Solstice Tools, contact TD!')
         print(str(e))
